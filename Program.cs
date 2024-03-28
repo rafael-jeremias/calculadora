4dca4d87494c06c48f61b22b838f3c87460911c4
Rafael Jeremias <rafael.jeremias.vs@outlook.com>
2024-03-28 19:48:47 -0300

    Projeto Calculadora

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace CalculadoraTabajara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            double primeiroN, segundoN;
+
+            Console.WriteLine("Calculadora Tabajara");
+
+            Console.WriteLine("Digite o primeiro número:");
+            primeiroN = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            segundoN = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Qual operação deseja realizar?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+
+           
+            switch (Console.ReadLine())
+
+            {
+                case "1":
+                        Console.WriteLine($"O resultado é {primeiroN + segundoN}"); 
+                    break;
+
+                case "2":
+                        Console.WriteLine($"O resultado é {primeiroN - segundoN}");
+                    break;
+
+                case "3":
+                        Console.WriteLine($"O resultado é {primeiroN * segundoN}");
+                    break;
+
+                case "4":
+                        Console.WriteLine($"O resultado é {primeiroN / segundoN}");
+                    break;
+              
+                default: 
+                    break;
+
+
+            }
+            
+
+          
+
+            
+
+        }
+
+
+    }
+}
